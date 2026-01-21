cc0ef5876221b23084b5bf6b5bf5a9e7487f6e86
DrOmA-CoDeR <vasakaogon@gmail.com>
2026-01-21 18:32:57 +0300

    Питаемя исправить

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 let but = document.querySelectorAll('.btn'); //кнопки
 
 function change(event) {  // добавил параметр event
-    event.preventDefault(); // предотвращаем стандартное поведение
     but.forEach(b => {
         b.classList.toggle('btnon');
     });
