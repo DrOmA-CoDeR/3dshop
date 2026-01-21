1b88e5f7826b8889734e3c799387f8360bf452f3
DrOmA-CoDeR <vasakaogon@gmail.com>
2026-01-21 18:28:16 +0300

    Едит ДЖС

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 'use strict';
 let but = document.querySelectorAll('.btn'); //кнопки
 
-function change() {
-    but.forEach(b => {  //для всех применение
-        b.addEventListener('click', function() { //евентлистенер
-            b.classList.toggle('btnon');
-        });
+function change(event) {  // добавил параметр event
+    event.preventDefault(); // предотвращаем стандартное поведение
+    but.forEach(b => {
+        b.classList.toggle('btnon');
     });
 }
 
-change();
+but.forEach(btn => {
+    btn.addEventListener('click', change);
+});
